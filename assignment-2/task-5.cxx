a69b1349be2d41548d0c4059e4939fa1c4fcc398
Keleas <svyat01ad@gmail.com>
2018-04-01 14:10:15 +0300

    Добавил решение 2.5

--- assignment-2/task-5.cxx
+++ assignment-2/task-5.cxx
@@ -19,10 +19,50 @@
 
 ****************************************************************************/
 
-#include <iostream>
+#include <vector>
+#include <fstream>
 
-int main()
-{
+/******************************************************************************/
+
+using std::vector;
+using std::min;
+
+/******************************************************************************/
+
+void floyd(vector<vector<double>> & graph) {
+    for (size_t k = 0; k < graph.size(); ++k)
+        for (size_t i = 0; i < graph.size(); ++i)
+            for (size_t j = 0; j < graph.size(); ++j)
+                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j]);
+}
+
+/******************************************************************************/
+
+int main() {
+    std::ifstream fin("floyd.in");
+    std::ofstream fout("floyd.out");
+
+    unsigned n = 1;
+    fin >> n;
+    vector<vector<double>> graph(n, vector<double>(n, 0));
+    for (size_t i = 0; i < n; ++i)
+        for (size_t j = 0; j < n; ++j ){
+            double a = 101;
+            fin >> a;
+            graph[i][j] = a;
+        }
+
+    floyd(graph);
+
+    for (size_t i = 0; i < n; ++i ){
+        for (size_t j = 0; j < n; ++j)
+            fout << graph[i][j] << ' ';
+        fout << '\n';
+    }
+
+    fin.close();
+    fout.close();
+
     return 0;
 }
 
