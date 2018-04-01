b580197a92560130f077688d236deb79eed86a14
Keleas <svyat01ad@gmail.com>
2018-04-01 13:11:57 +0300

    Добавил решение 2.2

--- assignment-2/task-2.cxx
+++ assignment-2/task-2.cxx
@@ -46,9 +46,130 @@ USD/RUB: 0.02
 ****************************************************************************/
 
 #include <iostream>
+#include <vector>
 
-int main()
+/*****************************************************************/
+
+using std::vector;
+using std::cin;
+using std::cout;
+
+/*****************************************************************/
+
+class Graph {
+public:
+    Graph(unsigned verticesNumber);
+
+    void addEdge(int from, int to, double weight);
+
+    unsigned getSize() const;
+
+    void getNext(int vertex, vector<int> &vertices) const;
+    void getPrev(int vertex, vector<int> &vertices) const;
+
+    double getWeight(int v, int u) const;
+
+private:
+    size_t size_;
+    vector<vector<double>> matr_;
+};
+
+/****************************GRAPH********************************/
+
+Graph::Graph(unsigned int size) :
+        size_(size),
+        matr_(size_, vector<double>(size_, -1)) {}
+
+
+void Graph::addEdge(int from, int to, double weight)
 {
+    matr_[from][to] = weight;
+}
+
+
+unsigned Graph::getSize() const
+{
+    return size_;
+}
+
+
+void Graph::getNext(int vertex, vector<int> &vertices) const
+{
+    vertices.clear();
+    for (size_t i = 0; i < size_; i++)
+        if (matr_[vertex][i] != -1)
+            vertices.push_back(i);
+}
+
+
+void Graph::getPrev(int vertex, vector<int> &vertices) const
+{
+    vertices.clear();
+    for (size_t i = 0; i < size_; i++)
+        if (matr_[i][vertex] != -1)
+            vertices.push_back(i);
+}
+
+
+double Graph::getWeight(int v, int u) const
+{
+    return matr_[v][u];
+}
+
+/*****************************************************************/
+
+// Вренуть true, если существвует арбитраж
+// T = O(|E||V|^2) (O(|V|^4) для графа на матрице
+// Алгоритм Беллмана-Форда обнаруживает только отрицательные циклы
+// который может быть достигнут из начальной вершины,
+// поэтому он запускается для каждой вершины графика
+bool arbitrage(const Graph &graph) {
+    // Запуск алгоритма Б.Ф. для каждой вершины
+    for (size_t k = 0; k < graph.getSize(); ++k) {
+        // Инициализация вектора стоимости i-ых валют
+        // в начальной валюте
+        vector<double> d(graph.getSize(), -1.0);
+        // Начальная валюта
+        d[k] = 1.0;
+        vector<int> vertices(graph.getSize());
+        // Реалаксирование вершин
+        for (size_t i = 0; i < graph.getSize() - 1; ++i) {
+            for (size_t v = 0; v < graph.getSize(); ++v) {
+                graph.getPrev(v, vertices);
+                for (int u : vertices)
+                    if (d[u] != -1.0 && d[v] < d[u] * graph.getWeight(u, v))
+                        d[v] = d[u] * graph.getWeight(u, v);
+            }
+        }
+        // Поиск отрицательного цикла
+        for (size_t v = 0; v < graph.getSize(); ++v) {
+            graph.getPrev(v, vertices);
+            for (int u : vertices)
+                if (d[u] != -1.0 && d[v] < d[u] * graph.getWeight(u, v))
+                    return true; // Вернуть true в случае успеха
+        }
+    }
+    // Вернуть false, если отрицательный цикл не найден
+    return false;
+}
+
+/******************************************************************************/
+
+int main() {
+    size_t n = 0;
+    cin >> n;
+    Graph graph(n);
+    for (size_t i = 0; i < n; ++i) {
+        for (size_t j = 0; j < n; ++j) {
+            if (i == j)
+                continue;
+            double x;
+            cin >> x;
+            graph.addEdge(i, j, x);
+        }
+    }
+
+    cout << ((arbitrage(graph))?"YES":"NO");
     return 0;
 }
 
